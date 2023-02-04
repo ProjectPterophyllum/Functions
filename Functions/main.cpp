1ae179e024efca04f3b422d84cd73e0ada01a88f
Alexey Bolshakov <bad2600@outlook.com>
2023-02-04 16:14:52 +0300

    Add code

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
 
-
+int Sum(int a, int b)
+{
+	return a + b;
+}
+int Dif(int a, int b)
+{
+	return a - b;
+}
 void main()
 {
-
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите числа: "; cin >> a >> b;
+	int c = Sum(a, b);
+	cout << "Сумма чисел: " << c;
+	cout << "\nРазность чисел: " << Dif(a, b);
 }
