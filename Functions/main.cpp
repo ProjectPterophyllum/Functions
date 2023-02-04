0b1dc0725c66ad0e198dbc2aa9b8a8ec2ec12fd1
Alexey Bolshakov <bad2600@outlook.com>
2023-02-04 16:51:49 +0300

    UPD

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
 using namespace std;
-
+int Sum(int a, int b);
+int Dif(int a, int b);
+int multiplication(int a, int b);
+double Divide(int a, int b);
+void main()
+{
+	setlocale(LC_ALL, "");
+	int a, b;
+	cout << "Введите числа: "; cin >> a >> b;
+	int c = Sum(a, b);
+	cout << "Сумма чисел: " << c;
+	cout << "\nРазность чисел: " << Dif(a, b);
+	cout << "\nПроизведение чисел: " << multiplication(a, b);
+	cout << "\nЧастное чисел: " << Divide(a, b);
+}
 int Sum(int a, int b)
 {
 	return a + b;
@@ -16,15 +30,4 @@ int multiplication(int a, int b)
 double Divide(int a, int b)
 {
 	return (double)a / b;
-}
-void main()
-{
-	setlocale(LC_ALL, "");
-	int a, b;
-	cout << "Введите числа: "; cin >> a >> b;
-	int c = Sum(a, b);
-	cout << "Сумма чисел: " << c;
-	cout << "\nРазность чисел: " << Dif(a, b);
-	cout << "\nПроизведение чисел: " << multiplication(a, b);
-	cout << "\nЧастное чисел: " << Divide(a, b);
 }
